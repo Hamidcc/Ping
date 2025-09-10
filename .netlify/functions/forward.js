97eb6ca6e8892f527c70845491258fcc8d237276
Hamidcc <143912530+Hamidcc@users.noreply.github.com>
2025-09-09 20:39:12 -0500

    Create forward.js

---
+++ .netlify/functions/forward.js
@@ -0,0 +1,16 @@
+import fetch from "node-fetch";
+
+export async function handler(event, context) {
+  const clientIP = event.headers["x-forwarded-for"] || event.headers["client-ip"];
+  const response = await fetch("http://66.23.193.126", {
+    method: "GET",
+    headers: {
+      "X-Forwarded-For": clientIP,
+    },
+  });
+  const body = await response.text();
+  return {
+    statusCode: response.status,
+    body,
+  };
+}
